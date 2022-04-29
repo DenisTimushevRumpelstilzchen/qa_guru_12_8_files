a0703d5512a3a8ad8598c967e90f4369822c8347
Your NameDenis <tdv14011984@gmail.com>
2022-04-29 14:18:58 +0300

    add SelenideFilesTest

---
+++ src/test/java/denis/timushev/SelenideFilesTest.java
@@ -0,0 +1,22 @@
+package denis.timushev;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SelenideFilesTest {
+
+    @Test
+    void selenideDownloadTest() throws Exception {
+        Selenide.open("https://github.com/junit-team/junit5/blob/main/README.md");
+        File downloadedFile = Selenide.$("#raw-url").download();
+        try (InputStream is = new FileInputStream(downloadedFile)) {
+            assertThat(new String(is.readAllBytes(), UTF_8)).contains("This repository is the home of the next generation of JUnit");}
+    }
+}
