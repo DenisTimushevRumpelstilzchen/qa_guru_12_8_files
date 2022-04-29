0c0ba7800e69da5b3ec109e79aa3025650d9e38a
Your NameDenis <tdv14011984@gmail.com>
2022-04-29 15:23:00 +0300

    updated build.gradle

---
+++ src/test/java/denis/timushev/FilesParsingTest.java
@@ -0,0 +1,21 @@
+package denis.timushev;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
+
+public class FilesParsingTest {
+
+    @Test
+    void parsePdfTest() throws Exception {
+        Selenide.open("https://junit.org/junit5/docs/current/user-guide/");
+        File pdfDownload = Selenide.$(byText("PDF download")).download();
+        PDF pdf = new PDF(pdfDownload);
+        assertThat(pdf.author).contains("Marc Philipp");
+        assertThat(pdf.numberOfPages).isEqualTo(166);
+    }
+}
