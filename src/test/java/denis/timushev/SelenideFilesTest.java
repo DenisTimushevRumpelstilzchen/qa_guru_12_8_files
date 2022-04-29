9d20893c082fea87937456b86bd5b51cf5bebff1
Your NameDenis <tdv14011984@gmail.com>
2022-04-29 15:00:34 +0300

    add SelenideFilesTest

--- src/test/java/denis/timushev/SelenideFilesTest.java
+++ src/test/java/denis/timushev/SelenideFilesTest.java
@@ -1,5 +1,6 @@
 package denis.timushev;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
@@ -12,11 +13,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SelenideFilesTest {
 
-    @Test
+    @Test // скачивание
     void selenideDownloadTest() throws Exception {
         Selenide.open("https://github.com/junit-team/junit5/blob/main/README.md");
         File downloadedFile = Selenide.$("#raw-url").download();
         try (InputStream is = new FileInputStream(downloadedFile)) {
-            assertThat(new String(is.readAllBytes(), UTF_8)).contains("This repository is the home of the next generation of JUnit");}
+            assertThat(new String(is.readAllBytes(), UTF_8)).
+                    contains("This repository is the home of the next generation of JUnit");}
+    }
+
+    @Test // загрузка
+    void uploadSelenideTest() {
+        Selenide.open("https://the-internet.herokuapp.com/upload");
+        Selenide.$("input[type='file']")
+                .uploadFromClasspath("files/1.txt");
+        Selenide.$("#file-submit").click();
+        Selenide.$("div.example").shouldHave(Condition.text("File Uploaded!"));
+        Selenide.$("#uploaded-files").shouldHave(Condition.text("1.txt"));
     }
 }
